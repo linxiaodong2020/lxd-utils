5736915ccb1ab26cb80b8dd970aad27474215e23
Sally <xiaodong.lin@aihuishou.com>
2020-08-26 11:45:10 +0800

    增加判断空对象

--- lib/functions.ts
+++ lib/functions.ts
@@ -1,3 +1,10 @@
+
+function _isPrototype(value: any) {
+  const Ctor = value && value.constructor
+  const proto = (typeof Ctor === 'function' && Ctor.prototype) || Object.prototype
+  return value === proto
+}
+
 /**
  * @description 查询字符串，获取url中参数
  *
@@ -28,4 +35,33 @@ export function sleep(timer: number): any {
       resolve(true);
     }, timer);
   });
+}
+export function isEmpty(value: any): boolean {
+  if (value === null) {
+    return true;
+  }
+  // 数组和字符串
+  if ((Array.isArray(value) || typeof value === "string")) {
+    return !value.length;
+  }
+  // 数字处理
+  if (typeof value === "number") {
+    return false;
+  }
+  // Map和Set对象处理
+  const tag = value.toString();
+  if (tag == '[object Map]' || tag == '[object Set]') {
+    return !value.size
+  }
+  // 原型对象处理
+  if (_isPrototype(value)) {
+    return !Object.keys(value).length
+  }
+  // 普通对象处理
+  for (const key in value) {
+    if (Object.prototype.hasOwnProperty.call(value, key)) {
+      return false
+    }
+  }
+  return true;
 }
