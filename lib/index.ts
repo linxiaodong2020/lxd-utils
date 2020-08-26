a4476820e4f4ab726275d67290b51745a615fddd
Sally <xiaodong.lin@aihuishou.com>
2020-08-26 12:04:30 +0800

    feat:增加集体导出

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,20 @@
-export * from "./functions";
 export * as RegExpRule from "./regexp";
+export * from "./functions";
 export * from "./device";
 export * from "./date";
 export * from "./price";
+// 引入出index.js之外的全部文件
+// const context = require.context('./', false, /\.ts$/);
+
+// const keys = context.keys().filter((item: string) => item !== './index.ts');
+
+// const utils: any = {};
+
+// keys.forEach((item: string) => {
+//   const match = item.match(/([^/]+)\.js$/);
+//   if (match) {
+//     const attr: string = match[1];
+//     utils[attr] = context(item);
+//   }
+// });
+// export default utils;
